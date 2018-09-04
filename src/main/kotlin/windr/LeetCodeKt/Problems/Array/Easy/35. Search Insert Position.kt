5fae209ea5e4dc1a41678c1f834148b97c9dcd0e
windr00 <imrwz@icloud.com>
2018-09-04 12:39:58 +0800

    simplified solution for problem 35

--- src/main/kotlin/windr/LeetCodeKt/Problems/Array/Easy/35. Search Insert Position.kt
+++ src/main/kotlin/windr/LeetCodeKt/Problems/Array/Easy/35. Search Insert Position.kt
@@ -6,13 +6,11 @@ import windr.LeetCodeKt.Problems.Solution
  * @see <a href="https://leetcode.com/problems/search-insert-position/description/">35. Search Insert Position</a>
  */
 fun Solution.searchInsert(nums: IntArray, target: Int): Int {
-    if (target <= nums[0]) {
-        return 0
-    }
-    for (i in 1 until nums.size) {
-        if (nums[i] == target || (nums[i] > target && nums[i - 1] < target)) {
+    val length = nums.size
+    for (i in 0 until length) {
+        if (nums[i] >= target) {
             return i
         }
     }
-    return nums.size
+    return length
 }
